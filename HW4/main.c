02c9d0ac110887298b7c70e227eea572f215f162
Arshak Poghosyan <arshakpoghosyan@SNOT00725s-MacBook-Pro.local>
2025-09-24 21:50:28 +0400

    modified

--- HW4/main.c
+++ HW4/main.c
@@ -2,8 +2,8 @@
 #include "math_utils.h"
 
 int main() {
-    int num = 5;
-    int result = square(num);
-    printf("The square of %d is %d\n", num, result);
+    int num = 12;
+    int sqr = square(num);
+    printf("Square of %d: %d\n", num, sqr);
     return 0;
 }
