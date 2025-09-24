11eda1f9a9230670de2e6e07b5bb23dafb5145e9
Arshak Poghosyan <arshakpoghosyan@SNOT00725s-MacBook-Pro.local>
2025-09-24 21:42:22 +0400

    HW4 added

---
+++ HW4/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main() {
+    int num = 5;
+    int result = square(num);
+    printf("The square of %d is %d\n", num, result);
+    return 0;
+}
---
+++ HW4/math_utils.c
@@ -0,0 +1,5 @@
+#include "math_utils.h"
+
+int square(int x) {
+    return x*x;
+}
